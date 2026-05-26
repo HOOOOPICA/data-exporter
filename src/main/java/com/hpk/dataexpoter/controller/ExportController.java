9e1f7bd2f9fddbb8d6ca16c487f8543ab22de529
HOOPICAAA <hoopicaaa@gmail.com>
2026-05-27 00:26:27 +0800

    feat:导出excel

--- src/main/java/com/hpk/dataexpoter/controller/ExportController.java
+++ src/main/java/com/hpk/dataexpoter/controller/ExportController.java
@@ -1,12 +1,18 @@
 package com.hpk.dataexpoter.controller;
 
+import com.alibaba.excel.EasyExcel;
 import com.hpk.dataexpoter.model.Order;
 import com.hpk.dataexpoter.service.ExportService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class ExportController {
@@ -22,4 +28,30 @@ public class ExportController {
     public List<Order> queryAllParallel() throws Exception {
         return exportService.queryAllOrdersParallel();
     }
+
+    /**
+     * 一次性查出所有数据，再写excel
+     * @param response
+     * @throws ExecutionException
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    @GetMapping("/export/excel")
+    public void exportExcel(HttpServletResponse response) throws ExecutionException, InterruptedException, IOException {
+        // 告诉浏览器，这是一个Excel文件下载
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码
+        String filename = URLEncoder.encode("订单信息", StandardCharsets.UTF_8);
+        response.setHeader("Content-Disposition", "attachment;filename=" + filename + ".xlsx");
+
+        // 多线程查询
+        List<Order> orders = exportService.queryAllOrdersParallel();
+        EasyExcel.write(response.getOutputStream(), Order.class)
+                .sheet("订单数据")
+                .doWrite(orders);
+    }
+
+
+    // todo 边读边写; 单线程版 vs 多线程版
 }
--- src/main/java/com/hpk/dataexpoter/model/Order.java
+++ src/main/java/com/hpk/dataexpoter/model/Order.java
@@ -1,5 +1,6 @@
 package com.hpk.dataexpoter.model;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -13,11 +14,17 @@ import java.time.LocalDateTime;
 public class Order {
 
     @TableId(type = IdType.AUTO)
+    @ExcelProperty("ID")
     private Long id;
 
+    @ExcelProperty("Order No.")
     private String orderNo;
+    @ExcelProperty("Customer Name")
     private String customerName;
+    @ExcelProperty("Amount")
     private BigDecimal amount;
+    @ExcelProperty("Status")
     private Integer status;
+    @ExcelProperty("Created At")
     private LocalDateTime createdAt;
 }
