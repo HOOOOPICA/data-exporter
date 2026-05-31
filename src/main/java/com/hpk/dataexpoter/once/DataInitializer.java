8b7b3790777f629a6771deffc6426abefa9b6907
HOOPICAAA <hoopicaaa@gmail.com>
2026-05-31 15:39:59 +0800

    fix: 补充export_task的初始化

--- src/main/java/com/hpk/dataexpoter/once/DataInitializer.java
+++ src/main/java/com/hpk/dataexpoter/once/DataInitializer.java
@@ -39,6 +39,10 @@ public class DataInitializer implements CommandLineRunner {
             System.out.println("products 表不存在，正在创建表结构...");
             createProductsTable();
         }
+        if(!tableExists("export_task")){
+            System.out.println("export_task 表不存在，正在创建表结构...");
+            createExportTaskTable();
+        }
     }
 
     private boolean seedOrdersIfEmpty() {
@@ -164,6 +168,19 @@ public class DataInitializer implements CommandLineRunner {
                     order_no      VARCHAR(32) NOT NULL,
                     product_name  VARCHAR(128) NOT NULL
                 )
+                """);
+    }
+
+    private void createExportTaskTable() {
+        jdbcTemplate.execute("""
+                CREATE TABLE export_task (
+                          id            BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          status        TINYINT      NOT NULL DEFAULT 0,  -- 0待处理 1进行中 2成功 3失败
+                          file_path     VARCHAR(255),                     -- 生成的文件路径
+                          error_message VARCHAR(500),                     -- 失败时记录原因
+                          created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,  -- 任务创建时间
+                          finished_at   TIMESTAMP                         -- 任务完成时间（你说的export_time）
+                 );
                 """);
     }
 
